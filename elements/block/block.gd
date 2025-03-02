2ba4b93829287f41985ce825beaf751015644a7e
AleksandrStepa <by_step@list.ru>
2025-03-02 21:03:34 +0200

    block explosion added

--- elements/block/block.gd
+++ elements/block/block.gd
@@ -10,12 +10,18 @@ var is_falling := false
 var destination_position := global_position
 var rng := RandomNumberGenerator.new()
 
+const EXPLOSION_TIME := 1
+
 @onready var lable := $Label
+@onready var sprite := $Sprite2D
+@onready var block_body := $StaticBody2D
+@onready var collision_shape := $CollisionShape2D
+@onready var bloc_particles := $BlockParticles2D
 
 func _ready():
 	durability = rng.randi_range(min_durability, max_durability)
 	lable.text = str(durability)
-	
+
 	
 func fall():
 	destination_position = global_position
@@ -35,5 +41,14 @@ func _on_body_entered(body: CharacterBody2D):
 	durability -= 1
 	lable.text = str(durability)
 	if durability == 0 and not is_hidet:
+		sprite.hide()
+		lable.visible = false
+		block_body.queue_free()
+		collision_shape.disabled = true
+		
+		var tween := create_tween()
+		tween.parallel().tween_property(bloc_particles, "modulate", Color("ffffff00"), EXPLOSION_TIME)
+		bloc_particles.emitting = true
 		is_hidet = true
+		await get_tree().create_timer(2).timeout
 		queue_free()
